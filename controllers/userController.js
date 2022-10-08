7ccfc006bd166cee8bb22a3a95bc32d182c7de12
Christopher White <x0tion007@gmail.com>
2022-10-08 19:27:46 +0100

    registration system completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,13 @@
 const asyncHandler = require('../middleware/asyncHandler')
 const User = require('../models/UserModel')
 const jwt = require('jsonwebtoken')
-
+const bcrypt = require('bcryptjs')
 // generate token
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET,
         {expiresIn: "1d"}
         )
 }
-
-
 
 const registerUser = asyncHandler(async(req,res) => {
     const {name,password,email} = req.body
@@ -28,17 +26,22 @@ const registerUser = asyncHandler(async(req,res) => {
         res.status(400)
         throw new Error("User with this email exist")
        }
-
     //     create new user 
     const saveUser = await User.create({
         name,
         email,
         password
     })
-
             ///generate token
             const token = generateToken(saveUser._id)
-
+            // send http cookie only
+            res.cookie("token",token,{
+                path:"/",
+                httpOnly: true,
+                expires: new Date(Date.now() + 1000 * 86400), // 1 day
+                sameSite: "none",
+                secure: true
+            })
     if(saveUser){
         const {_id, name, email,bio, photo, phone } = saveUser
         res.status(201).json({
@@ -50,6 +53,48 @@ const registerUser = asyncHandler(async(req,res) => {
     }
 })
 
+
+// login User
+const loginUser = asyncHandler(async(req,res) => {
+        const {email, password} = req.body
+
+        // validation of reqest
+        if(!email || !password){
+             res.status(400);
+             throw new Error("please filled in the require fields")
+        }
+        // if user exist
+        const user = await User.findOne({ email })
+        if(!user){
+            res.status(400);
+            throw new Error("user not found please Sign up")
+       }
+    //    if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+     ///generate token
+     const token = generateToken(user._id)
+     // send http cookie only
+     res.cookie("token",token,{
+         path:"/",
+         httpOnly: true,
+         expires: new Date(Date.now() + 1000 * 86400), // 1 day
+         sameSite: "none",
+         secure: true
+     })
+    // get the user informations
+    if(user && passwordIsCorrect){
+        const {_id, name, email,bio, photo, phone } = user
+        res.status(200).json({
+           _id,name,email,bio, photo, phone,token
+        });       
+    } else {
+        res.status(400);
+            throw new Error("Invalide email or password")
+    }
+})
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const cookieParser = require('cookie-parser')
 // importin Router
 const UserRoute = require('./routes/userRoute')
 // loading the env file
@@ -15,6 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 // assign port 
 const PORT = process.env.PORT || 3000
